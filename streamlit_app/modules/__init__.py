5a32200bd3a6af6bfe93fc24a034227a6d56c202
mrbeach160-bit <mrbeach160@gmail.com>
2025-08-08 14:53:17 +0700

    Revert "Refactor app.py Into Modular Architecture"

--- streamlit_app/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-# Modular Trading Bot Components
-# 
-# This package contains the refactored modules from the monolithic app.py
-# to provide better code organization and maintainability.
-
-__version__ = "1.0.0"
